33bd70af349fafc22aed4fce3b9b5a19a6a21213
msaadat03 <saadatam.code.edu.az>
2022-08-22 00:36:21 +0300

    HomeW14

---
+++ assets/js/script.js
@@ -0,0 +1,105 @@
+console.log("Her vaxtiniz xeyir, Cavid muellim)");
+
+//1) Verilmiş n - ədədinin 3-ə və 7-ə bölünüb-bölünməməsini tapan bir function yazin.
+// function Checking(n) {
+//     if( n %3 == 0 & n%7 == 0) {
+//     console.log("Делится и на 3, и на 7");
+//     } 
+//     else {
+//         console.log("Не делится на 3 и на 7");
+//     }
+
+// }
+// Checking(18);
+
+
+
+// 2) Functiona musbet bir eded gelir, hemin funksiya gelen ededin faktorialini hesablayib geri qaytarmalidir. 
+// let result = 1;
+// function findingFactorial(n) {
+//     if (n > 0) {
+//         for (let i = 1; i <= n; i++) {
+
+//             result *= i;
+//         }
+// console.log(result);
+// }
+// else {
+//     console.log("Введите положительное число");
+// }
+// }
+// findingFactorial(-2)
+
+
+
+// 3) Her hansisa number elementleri olan arayin icindeki cut ededlerin kvadratlarinin cemini hesablayan bir function yazin.
+// let array = [7, 17, 8, 1, 27, 28, 37, 38];
+// let sum = 0;
+
+// function SumEvenNum() {
+
+    
+//     for (let i = 0; i < array.length; i++) {
+//         if (array[i] % 2 == 0) {
+//             sum += array[i] * array[i]
+//         }
+//     }
+
+//     console.log(sum);
+// }
+// SumEvenNum(array);
+
+
+// 4) Funtiona-a  mail ve password gelir. Mail beraberdirse cavid@code.edu.az -a ve password beraberdirse 12345 -e, ekranda "Girish olundu" yazilsin, eks halda "Mail ve yaxud password sehvdir" yazilsin. 
+// let email = "sada@code.edu.az";
+// let pass = "1111111";
+
+// function CheckEmailPassword(mail, password) {
+//     if (email == mail && password == pass) {
+//         console.log("Авторизация прошла успешна");
+//     }
+//     else {
+//         console.log("Неправильный адрес электронной почты или пароль")
+//     }
+// }
+// CheckEmailPassword("saadatam@code.edu.az", "1111111")
+
+
+// 5) Functiona ededlerden ibaret array gelir, function hemin arraydaki tek ededlerin cemini qaytarmalidir.
+// let array = [1,4,8,7,9,11];
+// let sum = 0;
+
+// function sumOfOddNum(array){
+//     for (let i = 0; i < array.length; i++) {
+//         if (array[i]%2 != 0) {
+//             sum+=array[i]
+//         }
+//     }
+//     console.log(sum)
+// }
+// sumOfOddNum(array);
+
+
+// 6) Functiona ededlerden ibaret array gelir, function hemin arraydaki cut ededlerin sayini qaytarmalidir.
+// let array = [1,4,8,7,9,11];
+// let count = 0;
+
+// function countOfOddNum(array){
+//     for (let i = 0; i < array.length; i++) {
+//         if (array[i]%2 == 0) {
+//             count++
+//         }
+//     }
+//     console.log(count)
+// }
+// countOfOddNum(array);
+
+
+
+
+
+
+
+
+
+
